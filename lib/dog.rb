61f39a09f3ee598ce2e115d4c8e603659897a428
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-10 17:19:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,12 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
-    @@all << name
+    @@all << self
   end
   def self.all
-    puts @@all
+    @@all.each do |dog|
+      puts dog.name
+    end 
   end
   def self.clear_all
     @@all.clear
